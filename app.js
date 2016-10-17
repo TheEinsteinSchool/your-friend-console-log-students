26031087ed67185bfdf08f2d5c29fc9f72ddeb23
Cameron Banowsky <cameron@hack.cd>
2016-10-17 07:12:47 -0700

    assignment 1

---
+++ app.js
@@ -0,0 +1,9 @@
+// Create two variables one called firstName and the other lastName
+// Then log the variables to the console to print out the full name of the person you have defined.  We do this using console.log() inside our developer tools console.  Here is an example:
+// var pizza = cheese;
+// console.log("My favorite pizza is + " " " +  pizza)
+// This will print: My favorite pizza is cheese
+// Notice the semicolon after cheese.  After most expressions a semicolon is needed to move the code along to the next expression.
+// Now you try to print your first and last name  to the console. (look at my code above.  How do you think we will make  a space in between the two variables.  Good luck!
+
+// Start Below this line
